4e302e0b47735d797a356f870159739e49c8d2e4
overajhe92e <ardrey2200@gmail.com>
2025-10-23 12:18:36 -0400

    finn, shelly nerf, and misc (also dandy's here too)

--- items/jokers/dandy.lua
+++ items/jokers/dandy.lua
@@ -5,35 +5,77 @@ SMODS.Joker {
     config = {
         extra = {
             xmult = 1,
-            noitem = false,
+            noitem = true,
             anger = 0,
-            reset = 0
+            reset = 2,
+            ante_noitem = true
         }
     },
     blueprint_compat = true,
-    loc_vars = function(self,info_queue,card)
-        return { vars = { card.ability.extra.xmult }}
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.xmult, card.ability.extra.anger } }
     end,
-    calculate = function(self,card,context)
-        if context.buying_card then
+    calculate = function(self, card, context)
+        if context.buying_card and not context.blueprint then
             card.ability.extra.noitem = false
             card.ability.extra.anger = 0
             card.ability.extra.xmult = card.ability.extra.xmult + 0.25
-            return{message="X"..tostring(card.ability.extra.xmult).." Mult"}
+            return {
+                message = localize("k_dw_dandy_buy_" .. pseudorandom("dandybuy", 1, 6)) or
+                    localize("k_dw_dandy_stage3_buy_" .. pseudorandom("stage3dandybuy", 1, 6))
+            }
         end
         if context.joker_main then
             return {
                 xmult = card.ability.extra.xmult
             }
         end
-        if context.setting_blind and card.ability.extra.reset == 2 then
-            card.ability.extra.xmult = 1
-            return{message="Reset!"}
-        end
-        if context.ending_shop and card.ability.extra.noitem == true then
-            card.ability.extra.anger = card.ability.extra.anger + 1
-        elseif context.ending_shop and card.ability.extra.noitem == false then
+        if context.setting_blind and card.ability.extra.anger < 3 and not context.blueprint then
             card.ability.extra.noitem = true
+        elseif context.setting_blind and card.ability.extra.anger >= 3 and not context.blueprint then
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 1,
+                func = function()
+                    SMODS.destroy_cards( G.jokers.cards )
+                    return true
+                end
+            }))
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 1,
+                func = function()
+                    SMODS.add_card{ key = 'j_dw_tw_dandy' }
+                    return true
+                end
+            }))
+            
+        end
+        if context.end_of_round and context.game_over == false and context.main_eval and context.beat_boss and not context.blueprint then
+            card.ability.extra.reset = card.ability.extra.reset - 1
+        end
+        if context.end_of_round and context.game_over == false and context.main_eval and context.beat_boss and card.ability.extra.ante_noitem == true and not context.blueprint then
+            card.ability.extra.anger = card.ability.extra.anger + 1
+        end
+        if context.end_of_round and context.game_over == false and card.ability.extra.reset <= 1 and not context.blueprint then
+            card.ability.extra.xmult = 1
+            card.ability.extra.reset = 2
+            return { message = "Reset!" }
+        end
+        if context.ending_shop and card.ability.extra.noitem == true and not context.blueprint then
+            if card.ability.extra.anger == 1 then
+                return {
+                    message = localize("k_dw_dandy_stage1_" .. pseudorandom("stage1", 1, 6))
+                }
+            elseif card.ability.extra.anger == 2 then
+                return {
+                    message = localize("k_dw_dandy_stage2_" .. pseudorandom("stage2", 1, 6)) 
+                }
+            elseif card.ability.extra.anger == 3 then
+                return {
+                    message = localize("k_dw_dandy_stage3_" .. pseudorandom("stage3", 1, 6)) 
+                }
+            end
         end
     end
 }
@@ -49,18 +91,19 @@ SMODS.Joker {
         },
     },
     blueprint_compat = true,
-    loc_vars = function(self,info_queue,card)
-        return { vars = { card.ability.extra.xmult, card.ability.extra.wake_up_its_talisman_check_time * math.max(0, (G.GAME.dollars or 0) + (G.GAME.dollar_buffer or 0)) + 1 }}
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.xmult, card.ability.extra.wake_up_its_talisman_check_time * math.max(0, (G.GAME.dollars or 0) + (G.GAME.dollar_buffer or 0)) + 1 } }
     end,
-    calculate = function(self,card,context)
-        if context.buying_card then
+    calculate = function(self, card, context)
+        if context.buying_card and not context.blueprint then
             card.ability.extra.xmult = card.ability.extra.xmult + 1
-            return{message="X"..tostring(card.ability.extra.xmult).." Mult"}
+            return { message = "X" .. tostring(card.ability.extra.xmult) .. " Mult" }
         end
         if context.joker_main then
             return {
                 xmult = card.ability.extra.xmult,
-                emult = card.ability.extra.wake_up_its_talisman_check_time * math.max(0, (G.GAME.dollars or 0) + (G.GAME.dollar_buffer or 0)) + 1
+                emult = card.ability.extra.wake_up_its_talisman_check_time *
+                    math.max(0, (G.GAME.dollars or 0) + (G.GAME.dollar_buffer or 0)) + 1
             }
         end
     end
