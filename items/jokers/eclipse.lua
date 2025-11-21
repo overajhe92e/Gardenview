e90625da65471f3a533d41e19a1c52b83567088a
overajhe92e <ardrey2200@gmail.com>
2025-11-21 16:12:27 -0500

    fix later

--- items/jokers/eclipse.lua
+++ items/jokers/eclipse.lua
@@ -3,6 +3,13 @@ SMODS.Joker {
     rarity = 2,
     cost = 8,
 
-    calculate = function(self,card,context)
-    if nil then return nil end end
+    calculate = function(self, card, context)
+        if context.setting_blind then
+            if blind then
+                if pseudorandom_probability() then
+                    --disable blind code
+                end
+            end
+        end
+    end
 }
